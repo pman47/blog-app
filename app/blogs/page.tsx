7465338167bbb99356e614bbbc05f8da20b670f4
Manish Prajapati <manishprajapatim416@gmail.com>
2024-06-01 14:10:18 +0530

    Added search params to the page

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,24 +1,48 @@
-import { FC } from "react";
-import Image from "next/image";
-import { GetStaticProps } from "next";
-import path from "path";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import fs from "fs";
+import Image from "next/image";
+import path from "path";
 
-interface BlogPostsProps {}
+interface BlogPostsProps {
+  searchParams?: { query?: string };
+}
 
-async function getBlogPosts() {
+async function getBlogPosts(searchQuery: string = "") {
   const filePath = path.join(process.cwd(), "data", "blogPosts.json");
   const fileContents = fs.readFileSync(filePath, "utf8");
-  const blogPosts: BlogPost[] = JSON.parse(fileContents);
+  let blogPosts: BlogPost[] = JSON.parse(fileContents);
+
+  if (searchQuery) {
+    blogPosts = blogPosts.filter((blog) =>
+      blog.title.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+  }
 
   return blogPosts;
 }
 
-export default async function Page({}: BlogPostsProps) {
-  const blogPosts = await getBlogPosts();
+export default async function Page({ searchParams }: BlogPostsProps) {
+  const searchQuery = searchParams?.query || "";
+  const blogPosts = await getBlogPosts(searchQuery);
+
   return (
     <main className="container mx-auto flex flex-col md:px-24 md:my-20 flex-1">
       <h1 className="text-3xl font-bold">Blog</h1>
+      <form
+        className="mt-6 gap-2 flex flex-row items-center"
+        action="/blogs"
+        method="get"
+      >
+        <Input
+          type="text"
+          name="query"
+          placeholder="Search blog posts"
+          defaultValue={searchQuery}
+          className="flex-1"
+        />
+        <Button type="submit">Search</Button>
+      </form>
       <div className="w-full mt-9">
         <div className="w-full flex flex-col gap-6">
           {blogPosts.map((blog) => (
@@ -27,7 +51,14 @@ export default async function Page({}: BlogPostsProps) {
               key={blog.id}
             >
               <div className="aspect-video w-full sm:w-96 relative rounded-md overflow-hidden">
-                <Image src={blog.thumbnail} alt="work image" fill />
+                <Image
+                  src={blog.thumbnail}
+                  alt="work image"
+                  fill
+                  className="object-cover"
+                  sizes="(max-width: 640px) 100vw, 640px"
+                  priority
+                />
               </div>
               <div className="w-full flex flex-col gap-5">
                 <h2 className="text-3xl font-bold">{blog.title}</h2>
